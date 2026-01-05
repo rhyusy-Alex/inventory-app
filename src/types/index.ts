b38d1a68477302238d273757599699c43ed09836
유석용 <rhyusy@gmail.com>
2026-01-05 10:46:17 +0900

    Add created_at to type

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,6 +6,8 @@ export interface InventoryItem {
   total_qty: number;
   rented_qty: number;
   broken_qty: number;
-  // 가용 수량 (전체 - 대여 - 파손)
+  // 정렬에 사용되는 생성일자 (필수)
+  created_at: string;
+  // 가용 수량 (전체 - 대여 - 파손) - UI 계산용
   available_qty?: number; 
 }
